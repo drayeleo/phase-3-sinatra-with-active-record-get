a8da57a00b7d86bde996f78d5ce0118ec767af36
Dylan Raye-Leonard <drayeleonard@gmail.com>
2022-05-31 22:42:27 -0600

    commit tree bf07b70d264619255edaa84b95e17e785fcfc30b

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,38 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, "application/json"
 
-  get '/' do
+  get "/" do
     { message: "Hello world" }.to_json
   end
 
+  get "/games" do
+    games = Game.all.order(:title).limit(10)
+    games.to_json
+  end
+
+  # get "/games/:id" do
+  #   # look up the game in the database using its ID
+  #   game = Game.find(params[:id])
+  #   # send a JSON-formatted response of the game data
+  #   game.to_json
+  # end
+
+  get "/games/:id" do
+    game = Game.find(params[:id])
+
+    # include associated reviews in the JSON response
+    game.to_json(
+      only: %i[id title genre price],
+      include: {
+        reviews: {
+          only: %i[comment score],
+          include: {
+            user: {
+              only: [:name]
+            }
+          }
+        }
+      }
+    )
+  end
 end
